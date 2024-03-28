80c908d1460484910fa76dad6b9d9e785c1981ed
Maglio <magliocquirogaf@gmail.com>
2024-03-28 10:31:55 -0300

    avance del orm , falta probarlo

---
+++ librerias/orm.php
@@ -0,0 +1,46 @@
+<?php
+
+include "conexion.php";
+
+class Orm(){
+
+    protected $tabla;
+    protected $db;
+
+    public function __construct($tabla,$conn){
+
+        $this->tabla = $tabla;
+        $this->db = $conn;
+
+    }
+    public function selectall(){
+        $smt = $this->db->prepare("SELECT * FROM {$this->tabla}");
+        $smt->execute();
+        $smt->close();
+        return $smt->fetch_object();
+    }
+    public function selectby($id){
+        $smt = $this->db->prepare("SELECT * FROM {$this->tabla} WHERE id={$id}");
+        $smt->execute();
+        $smt->close();
+        return $smt->fetch_object();
+    }
+    public function insert($filas,$valores){
+        $smt = $this->db->prepare("INSERT INTO {$this->tabla} $filas VALUES $valores");
+        $smt->execute();
+        $smt->close();
+    }
+    public function delete($id){
+        $smt = $this->db->prepare("DELETE * FROM {$this->tabla} WHERE id={$id}");
+        $smt->execute();
+        $smt->close();
+    }
+    public function update($id,$valores){
+        $smt = $this->db->prepare("UPDATE {$this->tabla} SET $valores  WHERE id={$id}");
+        $smt->execute();
+        $smt->close();
+    }
+
+}
+
+?>
